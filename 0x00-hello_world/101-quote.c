2ddc164bb67d28b8d3781b7884c981000bbd79f7
Q-code-unique <queairs1@gmail.com>
2022-03-10 20:21:38 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 /**
  * main - Entry point
@@ -9,5 +9,6 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
 	return (1);
 }

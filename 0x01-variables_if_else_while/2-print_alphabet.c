c8b580c1c684e96aea4f1385f3c32e7d3ed51f30
Q-code-unique <queairs1@gmail.com>
2022-03-11 15:04:15 -0800

    Alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
 
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	        putchar("\n");
+		char low;
 
-	return (0);
+
+
+			for (low = 'a'; low <= 'z'; low++)
+
+						putchar(low);
+
+					printf("\n");
+
+
+
+						return (0);
+
 }
